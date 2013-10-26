43abd9308cbaee229942284c074637efbe0c8767
Patrick <pstanger@gmail.com>
2013-10-26 11:29:00 -0700

    Switched to postgres for production

--- Gemfile
+++ Gemfile
@@ -2,17 +2,21 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-contrib'
-gem 'sqlite3'
 gem 'activerecord', '~>3.2'
 gem 'sinatra-activerecord'
 gem 'bcrypt-ruby'
 #gem 'nokogiri'
 
 group :development, :test do
+  gem 'sqlite3'
   gem 'pry'
   gem 'rspec'
   gem 'thin'
   gem 'tux'
+end
+
+group :production do
+  gem 'pg'
 end
 
 gem 'rake'
--- shortly.rb
+++ shortly.rb
@@ -16,11 +16,24 @@ enable :sessions
 
 set :public_folder, File.dirname(__FILE__) + '/public'
 
-configure :development, :production do
+configure :development do
     ActiveRecord::Base.establish_connection(
        :adapter => 'sqlite3',
        :database =>  'db/dev.sqlite3.db'
      )
+end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
 end
 
 # Handle potential connection pool timeout issues
