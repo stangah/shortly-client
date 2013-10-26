7ee2bd153e1dacd4430d7ac537df06e5ba5dfccf
Patrick <pstanger@gmail.com>
2013-10-25 21:35:46 -0700

    sorting and filtering in angular- directive of linkview-and try login

---
+++ public-old/client/linkCreateView.js
@@ -0,0 +1,50 @@
+Shortly.LinkCreateView = Backbone.View.extend({
+
+  className: 'creator',
+
+  template: Templates['create'],
+
+  events: {
+    "submit": "shortenUrl"
+  },
+
+  render: function() {
+    this.$el.html( this.template() );
+    return this;
+  },
+
+  shortenUrl: function(e){
+    e.preventDefault();
+    var $form = this.$el.find('form .text');
+    var link = new Shortly.Link( {url: $form.val()} )
+    link.on('request', this.startSpinner, this);
+    link.on('sync',    this.success,      this );
+    link.on('error',   this.failure,      this );
+    $form.val('');
+    link.save();
+  },
+
+  success: function(link){
+    this.stopSpinner();
+    var view = new Shortly.LinkView( {model: link} );
+    this.$el.find('.message').append(view.render().$el.hide().fadeIn());
+  },
+
+  failure: function(){
+    this.stopSpinner();
+    this.$el.find('.message').html("Please enter a valid URL").addClass('error');
+  },
+
+  startSpinner: function(){
+    this.$el.find('img').show();
+    this.$el.find('form input[type=submit]').attr('disabled', "true");
+    this.$el.find('.message').html("").removeClass('error');
+  },
+
+  stopSpinner: function(){
+    this.$el.find('img').fadeOut('fast');
+    this.$el.find('form input[type=submit]').attr('disabled', null);
+    this.$el.find('.message').html("").removeClass('error');
+  }
+
+});
---
+++ public/client/app.js
@@ -0,0 +1,52 @@
+var shortly = angular.module('shortlyApp', [])
+.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    controller: 'linksCtrl',
+    // template:'<ul><li ng-repeat="link in links">{{ link }}</li>'
+    templateUrl: 'templates/link.html'
+  })
+  .when('/create', {
+    controller: 'createCtrl',
+    templateUrl: 'templates/create.html'
+
+  })
+  .otherwise({
+
+  });
+})
+.controller('linksCtrl',
+  function($scope, $http) {
+    $http.get('/links')
+    .success(function(data, status, header, config) {
+      $scope.links = data;
+    })
+    .error(function(data, status, header, config) {
+    });
+
+})
+.controller('createCtrl',
+  function($scope, $http) {
+    $scope.sendMsg = function() {
+      $scope.spinnerShow = true;
+      var urldata = JSON.stringify({url: $scope.link});
+      $http.post('/links', urldata)
+        .success(function(data, status) {
+          $scope.spinnerShow = false;
+          console.log(data);
+          $scope.message = data;
+        }).error(function(data, status) {
+          $scope.spinnerShow = false;
+          $scope.error = "Please enter a valid URL";
+        });
+    };
+}).directive('linkView', function() {
+  return {
+    // restrict: 'EA',
+    require: '^ngModel',
+    scope: {
+      ngModel: '=',
+    },
+    templateUrl: 'templates/linkView.html'
+  };
+});
